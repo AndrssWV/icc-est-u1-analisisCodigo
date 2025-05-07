3524de1dc254773337e22b86ebb7ace37191e28f
Andres Villalta <122935599+AndrssWV@users.noreply.github.com>
2025-05-06 20:49:40 -0500

    Updates

--- src_py/app.py
+++ src_py/app.py
@@ -1,11 +1,14 @@
 import benchmarking as bm
 from metodos_ordenamiento import MetodosOrdenamiento
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 if __name__ == "__main__":
     print('funciona')
     bench = bm.Benchmarking()
     metodos = MetodosOrdenamiento()
-    tamanos = [5000, 10000, 10500]
+    tamanos = [2500, 5000, 10000, 10500]
+    resultados = []
     for tam in tamanos:
         arreglo = bench.build_arreglo(tam)
         metodos_object = {
@@ -15,12 +18,32 @@ if __name__ == "__main__":
             "shell": metodos.sort_shell
         }
 
-        resultados = []
-
         for nombre, metodo in metodos_object.items():
             tiempo_resultado = bench.medir_tiempo(metodo, arreglo)
             tupla = (tam, nombre, tiempo_resultado)
             resultados.append(tupla)
 
-        for tam, nombre, tiempo in resultados:
-            print(f"Tamaño: {tam}, nombre metodo: {nombre}, tiempo: {tiempo:.6f} segundos")
+    for tam, nombre, tiempo in resultados:
+        print(f"Tamaño: {tam}, nombre metodo: {nombre}, tiempo: {tiempo:.6f} segundos")
+    
+    print(resultados)
+
+    tiempo_metodos = {
+        "burbuja": [],
+        "burbuja_mejorado": [],
+        "seleccion": [],
+        "shell": []
+    }
+
+    for tam, nombre, tiempo in resultados:
+        tiempo_metodos[nombre].append(tiempo)
+    
+    plt.figure(figsize=(10,6))
+    for nombre, tiempos in tiempo_metodos.items():
+        plt.plot(tamanos, tiempos, label=nombre, marker="o")
+    plt.title("Andres Villalta " + str(datetime.now()))
+    plt.xlabel("Tamaño arreglo")
+    plt.ylabel("Tiempo (segundos)")
+    plt.legend()
+    plt.show()
+    
